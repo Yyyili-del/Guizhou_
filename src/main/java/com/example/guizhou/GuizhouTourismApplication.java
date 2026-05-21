75b115ea63139604d8ba031bf99346ce0cba210a
Yyili <15885805044@163.com>
2026-05-21 19:52:31 +0800

    第二个项目

--- src/main/java/com/example/guizhou/GuizhouTourismApplication.java
+++ src/main/java/com/example/guizhou/GuizhouTourismApplication.java
@@ -2,8 +2,12 @@ package com.example.guizhou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.guizhou.entity")
+@EnableJpaRepositories(basePackages = "com.example.guizhou.repository")
 public class GuizhouTourismApplication {
 
     public static void main(String[] args) {
